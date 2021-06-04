81306f9526e2523fed6e4cdc0f3c8da02db50dbf
Federico Cecchetto <fedecek3@gmail.com>
2021-06-04 17:45:21 +0200

    Add version checking

--- src/manager/index.js
+++ src/manager/index.js
@@ -9,15 +9,28 @@ delete globalConfig.servers
 
 const servers = []
 
-const handleConnectError = (cfg, e) => {
-    console.log(`Connection to '${cfg.game}' server at ${cfg.host}:${cfg.port} failed: ${e}`)
-}
-
 process.on('uncaughtException', (err) => {
     console.log(`Caught unhandled exception: ${err.stack}`)
 })
 
+console.log('Checking for updates...')
 
+const hash = require('child_process').execSync('git rev-parse HEAD').toString().trim().substr(0, 8)
+const version = '0.1.0'
+const credits = `node-server-manager-v2 v${version} (${hash}) • fed`
+
+const commitId = require('child_process').execSync('git rev-parse HEAD').toString().trim()
+const lastCommit = require('child_process').execSync('git ls-remote https://github.com/fedddddd/node-server-manager-v2.git HEAD').toString().split(/\s+/g)[0].trim()
+
+io.print(commitId == lastCommit 
+    ? '^2node-server-manager-v2 is up to date^7' 
+    : `^3An update is available, run ^4\'git pull\'^3 to update^7`)
+
+io.print(`\nRunning ^4${credits}^7...\n`)
+
+const handleConnectError = (cfg, e) => {
+    io.print(`Connection to '^3${cfg.game}^7' server at ^3${cfg.host}:${cfg.port}^7 failed: ^1${e}`)
+}
 
 config.servers.forEach(async cfg => {
     const server = new Server({...globalConfig, ...cfg})
@@ -39,6 +52,6 @@ config.servers.forEach(async cfg => {
 
         server.start()
         loader.onLoad()
-        console.log(`Connected to '${cfg.game}' server at ${cfg.host}:${cfg.port}`)
+        io.print(`^2Connected^7 to '^3${cfg.game}^7' server at ^3${cfg.host}:${cfg.port}`)
     })
 })
