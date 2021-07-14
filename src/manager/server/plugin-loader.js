f0c2d917deb7952b7cb74d2426e377b4c8c6e679
Federico Cecchetto <fedecek3@gmail.com>
2021-07-14 23:26:12 +0200

    Catch all plugin errors

--- src/manager/server/plugin-loader.js
+++ src/manager/server/plugin-loader.js
@@ -14,18 +14,23 @@ class Loader {
         this.plugins = []
         this.pluginFiles = fs.readdirSync(path.join(__dirname, '../plugins'))
         this.pluginFiles.forEach(async (file) => {
-            const plugin = {name: file, timeoutHandles: [], intervalHandles: []}
-            const module = requireUncached(path.join(__dirname, `../plugins/${file}`))
-
-            module.onInterval = (callback, interval) => {
-                plugin.intervalHandles.push(setInterval(callback, interval))
+            try {
+                const plugin = {name: file, timeoutHandles: [], intervalHandles: []}
+                const module = requireUncached(path.join(__dirname, `../plugins/${file}`))
+    
+                module.onInterval = (callback, interval) => {
+                    plugin.intervalHandles.push(setInterval(callback, interval))
+                }
+                module.onTimeout = (callback, timeout) => {
+                    plugin.intervalHandles.push(setTimeout(callback, timeout))
+                }
+    
+                plugin.module = module
+                this.plugins.push(plugin)
             }
-            module.onTimeout = (callback, timeout) => {
-                plugin.intervalHandles.push(setTimeout(callback, timeout))
+            catch (e) {
+                console.log(`Error loading plugin ${file}: ${e}`)
             }
-
-            plugin.module = module
-            this.plugins.push(plugin)
         })
 
         process.on('exit', this.onUnload.bind(this))
@@ -46,7 +51,12 @@ class Loader {
                 : {name: filename, timeoutHandles: [], intervalHandles: []}
 
             if (plugin.module && typeof plugin.module.onUnload === 'function') {
-                plugin.module.onUnload()
+                try {
+                    plugin.module.onUnload(this.server)
+                }
+                catch (e) {
+                    console.log(`Error unloading plugin: ${plugin.name}: ${e}`)
+                }
             }
 
             plugin.timeoutHandles.forEach(handle => {
@@ -60,17 +70,27 @@ class Loader {
             plugin.timeoutHandles = []
             plugin.intervalHandles = []
 
-            plugin.module = requireUncached(path.join(__dirname, `../plugins/${filename}`))
+            try {
+                plugin.module = requireUncached(path.join(__dirname, `../plugins/${filename}`))
 
-            plugin.module.onInterval = (callback, interval) => {
-                plugin.intervalHandles.push(setInterval(callback, interval))
+                plugin.module.onInterval = (callback, interval) => {
+                    plugin.intervalHandles.push(setInterval(callback, interval))
+                }
+                plugin.module.onTimeout = (callback, timeout) => {
+                    plugin.intervalHandles.push(setTimeout(callback, timeout))
+                }
+    
+                if (typeof plugin.module.onLoad === 'function') {
+                    try {
+                        plugin.module.onLoad(this.server)
+                    }
+                    catch (e) {
+                        console.log(`Error loading plugin: ${plugin.name}: ${e}`)
+                    }
+                }
             }
-            plugin.module.onTimeout = (callback, timeout) => {
-                plugin.intervalHandles.push(setTimeout(callback, timeout))
-            }
-
-            if (typeof plugin.module.onLoad === 'function') {
-                plugin.module.onLoad(server)
+            catch (e) {
+                console.log(`Error loading plugin ${plugin.name}: ${e}`)
             }
         })
 
@@ -82,9 +102,19 @@ class Loader {
                 keys.forEach(async (key) => {
                     const name = key.toLowerCase()
                     if (typeof module[key] === 'function' && name == `on${event}` && name != 'onload' && name != 'onevent') {
-                        module[key].apply(null, args)
+                        try {
+                            module[key].apply(null, args)
+                        }
+                        catch (e) {
+                            console.log(`Error executing callback '${`on${event}`}' in plugin: ${plugin.name}: ${e}`)
+                        }
                     } else if (typeof module[key] === 'function' && name == 'onevent') {
-                        module[key](event, args)
+                        try {
+                            module[key](event, args)
+                        }
+                        catch (e) {
+                            console.log(`Error executing callback 'onevent' in plugin: ${plugin.name}: ${e}`)
+                        }
                     }
                 })
             })
@@ -96,7 +126,12 @@ class Loader {
 
         this.plugins.forEach(plugin => {
             if (typeof plugin.module.onLoad === 'function') {
-                plugin.module.onLoad(this.server)
+                try {
+                    plugin.module.onLoad(this.server)
+                }
+                catch (e) {
+                    console.log(`Error executing callback 'onload' in plugin: ${plugin.name}: ${e}`)
+                }
             }
         })
     }
@@ -105,8 +140,13 @@ class Loader {
         this.loaded = true
 
         this.plugins.forEach(plugin => {
-            if (typeof plugin.module.onUnload === 'function') {
-                plugin.module.onUnload(this.server)
+            if (typeof plugin.module.onUnload === 'function') { 
+                try {
+                    plugin.module.onUnload(this.server)
+                }
+                catch (e) {
+                    console.log(`Error executing callback 'onunload' in plugin: ${plugin.name}: ${e}`)
+                }
             }
         })
     }
