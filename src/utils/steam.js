b1ceea623830e243c0da6c77bd09588fb1603b5b
Federico Cecchetto <fedecek3@gmail.com>
2021-07-16 03:24:17 +0200

    Formatting

--- src/utils/steam.js
+++ src/utils/steam.js
@@ -9,14 +9,13 @@ const REGEX_STEAMID3  = /^\[U:1:[0-9]+\]$/
 
 const steamUtils = {
     toSteamID64: (steamID) => {
-        if (!steamID || typeof steamID !== "string") {
+        if (!steamID || typeof steamID !== 'string') {
             return false
-        }
-        else if (steamUtils.isSteamID3(steamID)) {
+        } else if (steamUtils.isSteamID3(steamID)) {
             steamID = steamUtils.fromSteamID3(steamID)
         }
 
-        const split = steamID.split(":")
+        const split = steamID.split(':')
         const v = BASE_NUM
         const z = split[2]
         const y = split[1]
@@ -24,17 +23,17 @@ const steamUtils = {
         return v.plus(z * 2).plus(y).toString()
     },
     isSteamID3: (steamID) => {
-        if(!steamID || typeof steamID !== "string") {
+        if (!steamID || typeof steamID !== 'string') {
             return false
         }
 
-        return REGEX_STEAMID3.test(steamID);
+        return REGEX_STEAMID3.test(steamID)
     },
     fromSteamID3: (steamID3) => {
-        const split = steamID3.split(":")
+        const split = steamID3.split(':')
         const last = split[2].substring(0, split[2].length - 1)
 
-        return "STEAM_0:" + (last % 2) + ":" + Math.floor(last / 2)
+        return 'STEAM_0:' + (last % 2) + ':' + Math.floor(last / 2)
     }
 }
 
